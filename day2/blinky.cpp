9b31013cf708bdd80ebcd1a02354a94ebbb93180
Kumar Abhishek <abhishek@theembeddedkitchen.net>
2019-06-11 20:28:38 +0530

    Add trace to example
    Signed-off-by: Kumar Abhishek <abhishek@theembeddedkitchen.net>

--- day2/blinky.cpp
+++ day2/blinky.cpp
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "Vblinky.h"
-#include "verilated.h"
 #include <iostream>
+
+#include "verilated.h"
+#include "verilated_vcd_c.h"
+#include "Vblinky.h"
 
 using namespace std;
 
-void tick(Vblinky *tb) {
+void tick(int tickcount, Vblinky *tb, VerilatedVcdC *tfp) {
     tb->eval();
+    if (tfp) // 2ns before tick
+        tfp->dump(tickcount * 10 - 2);
     tb->i_clk = 1;
     tb->eval();
+    if (tfp) // 2ns before tick
+        tfp->dump(tickcount * 10);
     tb->i_clk = 0;
     tb->eval();
+    if (tfp) { // 5ns after tick
+        tfp->dump(tickcount * 10 + 5);
+        tfp->flush();
+    }
 }
 
 int main(int argc, char **argv) { 
@@ -22,11 +32,18 @@ int main(int argc, char **argv) {
 
     int last_led;
 
+    // Add trace functionality
+    Verilated::traceEverOn(true);
+    VerilatedVcdC *tfp = new VerilatedVcdC;
+    tb->trace(tfp, 99);
+    tfp->open("blinky_trace.vcd");
+    uint64_t tickcount = 0;
+
     // 1<<20 timesteps
     last_led = tb->o_led;
     for (int k=0; k<(1<<20); k++) {
         // Timer tick
-        tick(tb);
+        tick(++tickcount, tb, tfp);
 
         if (last_led != tb->o_led) {
             printf("k = %7d, led=%d\n", k, tb->o_led);
