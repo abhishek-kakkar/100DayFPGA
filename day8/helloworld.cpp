20c206247a973060065ec83339850f30e8faaed1
Kumar Abhishek <abhishek@theembeddedkitchen.net>
2019-06-17 20:45:44 +0530

    Day 8: Commit partial testbench
    Signed-off-by: Kumar Abhishek <abhishek@theembeddedkitchen.net>

---
+++ day8/helloworld.cpp
@@ -0,0 +1,18 @@
+#include <Vhelloworld.h>
+#include "uartsim.h"
+#include "testb.h"
+
+int main(int argc, char **argv) {
+    Verilated::commandArgs(argc, argv);
+    TESTB<Vhelloworld> *tb = new TESTB<Vhelloworld>;
+    UARTSIM *uart = new UARTSIM();
+    unsigned int baudclocks;
+
+    baudclocks = tb->m_core->o_setup;
+    uart->setup(baudclocks);
+
+    for (int clocks=0; clocks<16*32*baudclocks; clocks++) {
+        tb->tick();
+        (*uart)(tb->m_core->o_uart_tx);
+    }
+}
