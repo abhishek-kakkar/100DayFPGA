5babaca742b68e586efa58ebb9f7866c139a9a42
Kumar Abhishek <abhishek@theembeddedkitchen.net>
2019-06-23 17:28:37 +0530

    Day 14: Add FPGA synthesis into Makefile for Day 6 carry
    Signed-off-by: Kumar Abhishek <abhishek@theembeddedkitchen.net>

---
+++ day14/day6-carry/top.v
@@ -0,0 +1,23 @@
+module top(
+    input wire CLK,
+
+    output wire PIN_1,
+    output wire PIN_2,
+    output wire PIN_3,
+    output wire PIN_4,
+    output wire PIN_5,
+    output wire PIN_6,
+    output wire PIN_7,
+    output wire PIN_8,
+    
+    input wire PIN_9
+);
+
+    driver mydriver(
+        .i_clk(CLK),
+        .led({PIN_6, PIN_5, PIN_4, PIN_3, PIN_2, PIN_1}),
+        .sw(!PIN_9)
+    );
+
+    assign {PIN_7, PIN_8} = 2'b0;
+endmodule
